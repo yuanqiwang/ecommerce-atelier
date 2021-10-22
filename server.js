f70f16d7560dd5bd74083891fbf198dbbe999f2f
Yuanqi Wang <zoeywang.zw@gmail.com>
2021-10-22 16:11:29 -0500

    Modify server.js to serve the correct static file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 let app = express();
 let PORT = 1234;
 
-app.use(express.static('public'))
+app.use(express.static('client/dist'))
 
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`))
