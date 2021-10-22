832894e94fbe79de381cd2e28a73d400adfa2680
Yitong Liu <yitong414@gmail.com>
2021-10-21 23:06:01 -0400

    Initial setup

---
+++ client/src/app.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    }
+  }
+
+
+
+  render () {
+    return (<div>
+      <Overview />
+      <Related />
+      <QA />
+      <Review />
+    </div>)
+  }
+}
+
+
+ReactDOM.render(<App />, document.getElementById('app'));
