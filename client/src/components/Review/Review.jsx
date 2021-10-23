b108e5534aa1b4aac84ba193a116a4ab7aa0c8b0
Maggie Saldivia <maggiesaldivia@gmail.com>
2021-10-23 08:45:45 -0700

    test review component

---
+++ client/src/components/Review/Review.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+class Review extends React.Component {
+
+  render() {
+    return (
+      <div>This will be Ratings and Review section</div>
+    )
+  }
+
+}
+
+export default Review;
