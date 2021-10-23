b1ea011cb857c5ba50e8c7a4a49f57ccc26b348e
Adam <adam.a.lohnes@gmail.com>
2021-10-23 14:13:29 -0400

    Overview component 'hello world'

---
+++ client/src/components/Overview/Overview.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Overview extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello Team Ketchup!</h1>
+        <img src="https://i.pinimg.com/564x/1d/ab/b2/1dabb25c645161f596f0a70f1b715bae.jpg"/>
+      </div>
+    );
+  }
+}
+
+export default Overview;
