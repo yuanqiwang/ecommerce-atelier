c4d9f9fa5c1b1fc62b7eb3f339ccdae4940e81ab
Adam Lohnes <adam.a.lohnes+hackreactor@gmail.com>
2021-10-26 21:31:05 -0700

    Rough out overview components

---
+++ client/src/components/Overview/sub-components/ProductOptions.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ProductOptions(props) {
+  return (
+    <div id="overview_product_options">
+      <h3>condiment</h3>
+      <h2>Ketchup</h2>
+    </div>
+  );
+};
+
+export default ProductOptions;
