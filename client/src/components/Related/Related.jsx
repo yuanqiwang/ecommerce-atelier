e524038c305d5227f1c1d644ff99389970db6631
Yitong Liu <yitong414@gmail.com>
2021-10-29 19:26:47 -0400

    [Yitong] Setup basic horizontal scrolling

--- client/src/components/Related/Related.jsx
+++ client/src/components/Related/Related.jsx
@@ -1,23 +1,46 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import RP_sub from './related_components/RP-sub.jsx';
 import YO from './related_components/YO.jsx';
 
-const Related =(props) => {
+class Related extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.navRef = React.createRef();
+  }
 
 
-  return (
-    <div id='related'>
-      <br/>
-      <h2> Part 2 : This will be Related Products section</h2>
-      <h3>RELATED PRODUCTS</h3>
-      <section className = 'rp-cardrow'>
-        {props.relatedProductArr.map((item, i) => <RP_sub key={i} item ={item} /> )}
-      </section>
-      <YO />
-      <br/>
-    </div>
-  );
 
+  handleNav = (direction) => {
+    if(direction === 'left'){
+      console.log(this.navRef.current.style)
+      this.navRef.current.scrollLeft -= 280;
+    } else {
+      console.log(this.navRef.current)
+      this.navRef.current.scrollLeft += 280;
+    }
+  };
+
+  render(){
+    return (
+      <div id='related'>
+        <br/>
+        <h2> Part 2 : This will be Related Products section</h2>
+        <div id='title'>RELATED PRODUCTS</div>
+        <div id ='wrapper'>
+          <section className = 'rp-cardrow' ref={this.navRef}>
+            {this.props.relatedProductArr.map((item, i) => <RP_sub key={i} item ={item} /> )}
+            <div class="paddles">
+              <button class="left-paddle paddle hidden" onClick={()=>this.handleNav('left')}> {'<'} </button>
+              <button class="right-paddle paddle" onClick={()=>this.handleNav('right')}> {'>'} </button>
+            </div>
+          </section>
+        </div>
+        <YO />
+        <br/>
+      </div>
+      );
+  }
 }
 
 export default Related;
