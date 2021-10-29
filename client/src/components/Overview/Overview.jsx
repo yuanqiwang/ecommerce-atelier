4dce058f825063d431f4271d2ec3ad305659aec7
Adam Lohnes <adam.a.lohnes+hackreactor@gmail.com>
2021-10-28 20:48:11 -0700

    Implement hero pic functionality

--- client/src/components/Overview/Overview.jsx
+++ client/src/components/Overview/Overview.jsx
@@ -4,10 +4,23 @@ import ProductOptions from './sub-components/ProductOptions.jsx';
 import ProductInfo from './sub-components/ProductInfo.jsx';
 
 class Overview extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentImg: "https://pngimg.com/uploads/ketchup/ketchup_PNG21.png"
+    };
+
+    this.changeHeroPic = this.changeHeroPic.bind(this);
+  }
+
+  changeHeroPic(pic) {
+    this.setState({currentImg: pic});
+  }
+
   render() {
     return (
       <div id="overview">
-        <ImageGallery />
+        <ImageGallery heroPic={this.state.currentImg} changePic={this.changeHeroPic} />
         <ProductOptions />
         <ProductInfo />
       </div>
