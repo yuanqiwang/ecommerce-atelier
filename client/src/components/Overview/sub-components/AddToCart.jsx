30d8b850d8252ab618d496a5baa1eb6929637289
Adam Lohnes <adam.a.lohnes+hackreactor@gmail.com>
2021-10-31 14:14:48 -0700

    temporarily remove dummy button click

--- client/src/components/Overview/sub-components/AddToCart.jsx
+++ client/src/components/Overview/sub-components/AddToCart.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function AddToCart(props) {
   return (
     <div id="overview_add_cart">
-      <button onClick="">Add to Bag<span>+</span></button>
+      <button>Add to Bag<span>+</span></button>
       <div id="overview_favorite">☆</div>
     </div>
   );
