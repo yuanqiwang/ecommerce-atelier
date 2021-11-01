af5f52818a93ee5fc8398486c968c4163b6bb674
Yitong Liu <yitong414@gmail.com>
2021-11-01 14:02:39 -0400

    [Yitong] Decimal rating is working for 5-star rating!

--- client/src/components/Related/related_components/RP-sub.jsx
+++ client/src/components/Related/related_components/RP-sub.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios';
+import Style from 'style-it';
+
 
 const RP_sub = (props) => {
 
@@ -7,18 +9,37 @@ const RP_sub = (props) => {
   const[stylePic, setStylePicture] = useState([]);
   const[stylePrice, setStylePrice] = useState([]);
   const[styleName, setStyleName] = useState([]);
-  const[reviewInfo, setReview] = useState({});
+  const[reviewInfo, setReview] = useState(0);
 
+  const avgReview = (reviewObj) => {
+    let reviewNum=0;
+    let ratingSum=0;
+
+    if(reviewObj) {
+      let keys = Object.keys(reviewObj);
+      let values = Object.values(reviewObj);
+
+        for (var i=0; i<keys.length; i++) {
+          // console.log(keys[i], typeof keys[i]);
+          // console.log(values[i], typeof values[i])
+          reviewNum += parseInt(values[i]);
+          ratingSum += parseInt(keys[i])*parseInt(values[i]);
+        }
+    }
+
+
+    return reviewNum > 0?  (ratingSum/reviewNum).toFixed(1) : 0
+
+  };
 
   const getProductInfo = async () => {
     try {
       const response =  await axios.get(`/product/info/${props.item}`);
-
+      const avgReviewnum = avgReview(response.data['reviewStars'].ratings);
       setInfo(response.data['prod']);
       setStylePicture(response.data['style']['results'][0]['photos'][0]['thumbnail_url']);
-      setReview(response.data['reviewStars'].ratings)
+      setReview(avgReviewnum)
       setStylePrice(response.data['style']['results'][0]['original_price'])
-
       const ProductName = response.data['prod']['name'] + ' -- ' + response.data['style']['results'][0]['name'];
       setStyleName(ProductName)
 
@@ -37,9 +58,12 @@ const RP_sub = (props) => {
     <article className = 'rp-card'>
       <img className='rp-card-img' src={stylePic} />
       <div className ='sub-card'>
-        <div id = 'category'> &nbsp;{productInfo.category}</div>
-        <div id = 'name'> &nbsp;{styleName}</div>
-        <div id= 'price'> &nbsp;${stylePrice}</div>
+        <div id = 'category'> {productInfo.category}</div>
+        <div id = 'name'> {styleName}</div>
+        <div id= 'price'> {stylePrice}</div>
+      </div>
+      <div className ='sub-card-star' style = {{'--rating': reviewInfo}} >
+        &nbsp;{reviewInfo}
       </div>
     </article>
   )
@@ -48,3 +72,4 @@ const RP_sub = (props) => {
 
 
 export default RP_sub;
+//style = "--rating: 2.3"
