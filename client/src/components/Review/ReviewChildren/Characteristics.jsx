b51923c596bd01de38c093bcf4f2262a51ee7e9e
Maggie Saldivia <maggiesaldivia@gmail.com>
2021-11-18 08:59:56 -0800

    finish the characteristic breakdown

--- client/src/components/Review/ReviewChildren/Characteristics.jsx
+++ client/src/components/Review/ReviewChildren/Characteristics.jsx
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Traits from './Traits.jsx'
+import meanings from "./meanings.js";
+
+
+
 
 
 const Characteristics = (props) => {
   let category, value;
   let characArr = [];
+  let [trait, setTrait] = useState('')
   if (props.characteristics !== undefined) {
     category = Object.keys(props.characteristics)
     for (var i=0; i<category.length; i++) {
       characArr.push(category[i])
       characArr.push(parseFloat(props.characteristics[category[i]].value))
     }
+    console.log(meanings['Fit'])
     value = characArr.map((item, index) =>
       index%2 === 0 ?
       <h3 className="review-characteristic" key={index}>{item}</h3> :
-      <div className="rating-progress">
-        <div className="progress-bar-gray"></div>
-        <div className="progress-bar-white"></div>
-        <div className="progress-bar-gray"></div>
-        <div className="progress-bar-white"></div>
-        <div className="progress-bar-gray"></div>
+      <div>
+        <input type="range" min="1" max="100" value={(item / 5) * 100} id="myRange"></input>
+        <div class="container space-between">
+          <div>{meanings[characArr[index - 1]][0]}</div>
+          <div>{meanings[characArr[index - 1]][1]}</div>
+          <div>{meanings[characArr[index - 1]][2]}</div>
+        </div>
       </div>
     )
   }
+
   return (
     <>
     {value}
---
+++ client/src/components/Review/ReviewChildren/Traits.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import meanings from "./meanings.js";
+
+
+const Traits = (props) => {
+  const cats = props.cats
+  console.log(cats)
+  for (var i=0; i<cats.length; i++) {
+    return (
+      <div class="container space-between">
+        <div>{meanings[cats[i]][0]}</div>
+        <div>{meanings[cats[i]][2]}</div>
+        <div>{meanings[cats[i]][4]}</div>
+      </div>
+    )
+  }
+  return (
+    null
+  )
+
+}
+
+export default Traits
