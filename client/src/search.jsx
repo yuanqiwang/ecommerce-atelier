3b9dc3c0878b093897e17f68e3f0a699a3f93cdc
Maggie Saldivia <maggiesaldivia@gmail.com>
2021-11-17 22:56:40 -0800

    solving merge conflict in search.jsx

--- client/src/search.jsx
+++ client/src/search.jsx
@@ -1,10 +1,6 @@
 import React, {useState, useEffect} from 'react';
 
 const Search = ({onChange, placeholder}) => {
-<<<<<<< HEAD
-
-=======
->>>>>>> 6309ce81216760b4603c66313cabdb5aba305f49
   return (
     <div className='qa-search-bar' >
         <input
