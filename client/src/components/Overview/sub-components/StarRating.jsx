f4b22ec8789ba7343aeea8e88d25785080dfbb8f
Adam Lohnes <adam.a.lohnes+hackreactor@gmail.com>
2021-11-18 19:36:32 -0800

    connect star rating and style

--- client/src/components/Overview/sub-components/StarRating.jsx
+++ client/src/components/Overview/sub-components/StarRating.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import Star from '../../../star.jsx';
+import Star from '../../../star.jsx';
 
 function StarRating(props) {
   const scrollToReviews = () => {
@@ -7,24 +7,28 @@ function StarRating(props) {
     reviewsSection.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
   };
 
-  let avgRating = 0;
+  console.log('star props: ', props.stars);
 
-  if ( props.stars.ratings !== undefined ) {
-    let total = 0;
+  // let avgRating = 0;
 
-    for ( let rating of Object.keys(props.stars.ratings) ) {
-      // console.log(rating, props.stars.ratings[rating]);
-      total += parseInt(props.stars.ratings[rating])
-      avgRating += rating * parseInt(props.stars.ratings[rating]);
-    }
+  // if ( props.stars.ratings !== undefined ) {
+  //   let total = 0;
 
-    avgRating /= total;
-  }
+  //   for ( let rating of Object.keys(props.stars.ratings) ) {
+  //     // console.log(rating, props.stars.ratings[rating]);
+  //     total += parseInt(props.stars.ratings[rating])
+  //     avgRating += rating * parseInt(props.stars.ratings[rating]);
+  //   }
+
+  //   avgRating /= total;
+  // }
+
+  // console.log('avg: ', avgRating);
 
   return (
     <div id="overview_star_rating" data-testid="star-rating">
-      <div className="overview_stars" style={{'--rating': avgRating}}></div>
-      {/* <Star stars={props.stars.ratings} /> */}
+      {/* <div className="overview_stars" style={{'--rating': avgRating}}></div> */}
+      <Star stars={props.stars} />
       <div className='go-to-reviews' onClick={scrollToReviews}>Read all reviews</div>
     </div>
   );
